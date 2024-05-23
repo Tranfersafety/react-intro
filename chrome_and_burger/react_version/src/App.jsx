28c788a67c2df4842135b61811b5ede009352ef6
Kanthamas <114124443+Kanthamas@users.noreply.github.com>
2024-05-23 19:54:15 +0700

    add chrome_and_burger react_version

---
+++ chrome_and_burger/react_version/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<>
+			<h1 className="text-blue-400 font-bold text-6xl text-center my-0 mx-auto">
+				Hello React World!
+			</h1>
+		</>
+	);
+}
+
+export default App;
